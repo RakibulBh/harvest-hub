8be5ee260ae27942e4bc3889d5db19bd905be38c
RakibulBh <rakibulbhuiyan1973@gmail.com>
2025-01-07 16:59:03 +0000

    add: PROJECT STRUCTURE

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,9 @@
-import Navbar from "@/components/navbar";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="h-screen w-full bg-green-100">
-      {/* Nav bar */}
-      <Navbar />
-      <p>Hello</p>
+    <main>
+      <p>MAIN PAGE YET TO DEVELOP</p>
     </main>
   );
 }
